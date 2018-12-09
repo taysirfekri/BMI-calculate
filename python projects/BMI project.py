460f6719e85f12c353fa7719c0dc30903c320236
taysirfekri <taysirmohmed@gmail.com>
2018-12-09 06:33:12 +0200

    Update BMI project.py

--- python projects/BMI project.py
+++ python projects/BMI project.py
@@ -2,11 +2,11 @@ def BMI():
 	hight=input("how tall are you ?");
 	weight=input("what is your weight?");
 	bmi_value=int(weight)/(int(hight)/100)**2;
-	print('your BMI is {}'.format(round(bmi_value,2)));
+	print('your BMI is {} '.format(round(bmi_value,2)));
 	if bmi_value<18.5:
-		print('you must eat more!')
+		print('you must eat more')
 	elif bmi_value>=18.5 and bmi_value<=24:
 		print('good job bro ^_^')
 	else:
-		print("you need loss\'s some")
+		print("you need lose some weight ")
 BMI()
